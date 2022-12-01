5c92ff29ec3d082a7c5c357ecb9abcd7b5426217
fadlir18 <rachmanfadlir@gmail.com>
2022-12-01 17:19:34 +0700

    Add another menu

---
+++ cypress/e2e/1-auth/login.cy.js
@@ -0,0 +1,7 @@
+it.only('login test', function() {
+    cy.visit('https://cms.mysuperindo.co.id/')
+    cy.get(':nth-child(3) > .uk-inline > .uk-input').type('fadli')
+    cy.get('.parent-password-form > .uk-inline > .uk-input').click()
+    cy.get('.parent-password-form > .uk-inline > .uk-input').type('Brilliant77!')
+    cy.get('.uk-button.uk-width-1-1').click()
+})
--- cypress/e2e/1-auth/logout.cy.js
+++ cypress/e2e/1-auth/logout.cy.js
@@ -1,8 +1,11 @@
 it.only('login test', function() {
-
     cy.visit('https://cms.mysuperindo.co.id/')
     cy.get(':nth-child(3) > .uk-inline > .uk-input').type('fadli')
     cy.get('.parent-password-form > .uk-inline > .uk-input').click()
     cy.get('.parent-password-form > .uk-inline > .uk-input').type('Brilliant77!')
     cy.get('.uk-button.uk-width-1-1').click()
+    cy.wait(5000)
+    cy.get('.uk-navbar-toggle > img').click()
+    cy.get('.uk-navbar-dropdown > .uk-nav-default > :nth-child(2) > a').click()
+    cy.get('[data-v-62b040f7=""][data-v-d2cb0d50=""] > #modal-example > .uk-modal-dialog > .uk-text-center > .red').click()
 })
