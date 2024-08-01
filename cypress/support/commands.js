21a355762a0009995be6834b1051239b292b6ef5
fadlir18 <rachmanfadlir@gmail.com>
2024-08-01 13:46:46 +0700

    init

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -2,6 +2,7 @@
 Cypress.Commands.add('login', (username, password) => {
   cy.visit('/login')
   cy.get('#username').type(username)
+  
   cy.get('#password').click()
   cy.get('#password').type(password)
   cy.get('#submitButton').click()
